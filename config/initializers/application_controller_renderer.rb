41e4b7a6c2a7e88df3e6e2fd2c71dd1b914b1371
nakagawa-ren <hh.hakoware@gmail.com>
2021-07-09 10:20:47 +0000

    second

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ba93daa200cd45d7755aaed809474f89b525c168cd4f48fa3cd245b72bdacdc285bbd43aec0a33e48b36fc4382a09ea86b3f414f58f8af42c9728f6388ec775b'
